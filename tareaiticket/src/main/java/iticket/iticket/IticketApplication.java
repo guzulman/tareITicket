e6247b9a178bf2b93b2810327a21f7e5daf60937
guzulman <123052175+guzulman@users.noreply.github.com>
2023-03-01 21:46:36 -0600

    Inicialización ITicket

---
+++ tareaiticket/src/main/java/iticket/iticket/IticketApplication.java
@@ -0,0 +1,13 @@
+package iticket.iticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IticketApplication.class, args);
+	}
+
+}
---
+++ tareaiticket/src/main/java/iticket/iticket/repository/ArtistaRepository.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package iticket.iticket.repository;
+
+import iticket.iticket.entity.Artista;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+/**
+ *
+ * @author jcga5
+ */
+@Repository
+public interface ArtistaRepository extends CrudRepository<Artista, Long> {
+    
+}
